19d82f8e68461793613536b4bf7267856c9c9e2a
Guilherme Navarro <guilherme-navarro@outlook.com>
2023-08-24 10:28:38 -0300

    ADD: Created IProject and method to save

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,76 @@
 <template>
-    <h1>Projects</h1>
+    <section class="projects">
+        <h1 class="title">Projects</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="projectName" class="label">
+                    Project's Name
+                </label>
+                <input type="text" class="input" v-model="projectName" id="projectName">
+            </div>
+            <div class="field">
+                <button class="button is-success" type="submit">
+                    Save
+                </button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>
+                        Id
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="project in projects" :key="project.id">
+                    <td>
+                        {{ project.id }}
+                    </td>
+                    <td>
+                        {{ project.name }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IProject from "../interfaces/IProject";
 
 export default defineComponent({
-    name: 'Projects'
+    name: 'Projects',
+    data() {
+        return {
+            projectName: '',
+            projects: [] as IProject[]
+        }
+    },
+    methods: {
+        salvar() {
+            const project: IProject = {
+                name: this.projectName,
+                id: new Date()
+                    .toISOString()
+                    .replace(/\D/g, "")
+            }
+
+            this.projects.push(project)
+            this.projectName = ''
+        }
+    }
 })
 </script>
+
+
+<style scoped>
+.projects {
+    padding: 1.25rem
+}
+</style>
